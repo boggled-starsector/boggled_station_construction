d4ae649d974cef4217eb4d118e0d2945bf10ab44
boggled-starsector <56835214+boggled-starsector@users.noreply.github.com>
2019-11-26 10:19:38 -0500

    Delete BoggledStationConstructionIDs.java

--- Boggled Station Construction/src/data/campaign/econ/BoggledStationConstructionIDs.java
+++
@@ -1,9 +0,0 @@
-package data.campaign.econ;
-
-public class BoggledStationConstructionIDs {
-
-    public static class BoggledStationConstructionIndustryIDs
-    {
-        public static final String ASTROPOLIS_STATION = "ASTROPOLIS_STATION";
-    }
-}
